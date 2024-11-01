0a178a26a24eddd323eff81e334f6064ac646e4f
adhurimcanhasi22 <121344747+adhurimcanhasi22@users.noreply.github.com>
2024-11-01 13:57:38 +0100

    Add files via upload

---
+++ BonusScore/BonusScore.cs
@@ -0,0 +1,48 @@
+﻿class BonusScore
+{
+	static void Main()
+	{
+		int points = int.Parse(Console.ReadLine());
+
+		if (points <= 100 && points % 2 == 0)
+		{
+			int bonus = 6;
+			Console.WriteLine(bonus);
+			Console.WriteLine(bonus + points);
+		}
+		else if (points <= 100 && points % 5 == 0)
+		{
+			int bonus = 7;
+			Console.WriteLine(bonus);
+			Console.WriteLine(bonus + points);
+		}
+		else if (points <= 1000 && points > 100 && points % 2 == 0)
+		{
+			int bonus = ((20 * points) / 100) + 1;
+			Console.WriteLine(bonus);
+			Console.WriteLine(bonus + points);
+
+		}
+		else if (points <= 1000 && points > 100 && points % 5 == 0)
+		{
+			int bonus = ((20 * points) / 100) + 2;
+			Console.WriteLine(bonus);
+			Console.WriteLine(bonus + points);
+
+		}
+		else if (points > 1000 && points % 2 == 0)
+		{
+			int bonus = ((10 * points) / 100) + 1;
+			Console.WriteLine(bonus);
+			Console.WriteLine(bonus + points);
+
+		}
+		else if (points > 1000 && points % 5 == 0)
+		{
+			int bonus = ((10 * points) / 100) + 2;
+			Console.WriteLine(bonus);
+			Console.WriteLine(bonus + points);
+
+		}
+	}
+}
