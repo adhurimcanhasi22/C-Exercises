d608d1aa13af4ca2b026fdef63691b337ddff54a
Adhurim Canhasi <121344747+adhurimcanhasi22@users.noreply.github.com>
2024-11-01 15:06:24 +0100

    Add files via upload

---
+++ Equal3Numbers/Equal3Numbers.cs
@@ -0,0 +1,8 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+if (a == b && a == c && b == c)
+{
+    Console.WriteLine("yes");
+}
+else { Console.WriteLine("no"); }
---
+++ EvenOdd/EvenOdd.cs
@@ -0,0 +1,15 @@
+﻿internal class EvenOdd
+{
+    private static void Main()
+    {
+        double number = double.Parse(Console.ReadLine());
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Even!");
+        }
+        else
+        {
+            Console.WriteLine("Odd!");
+        }
+    }
+}
