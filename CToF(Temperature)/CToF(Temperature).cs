41407f9b871eb10fba1217994e28af4f60b778b3
Adhurim Canhasi <121344747+adhurimcanhasi22@users.noreply.github.com>
2024-11-01 14:59:53 +0100

    Add files via upload

---
+++ CToF(Temperature)/CToF(Temperature).cs
@@ -0,0 +1,14 @@
+﻿class Celcius
+{
+    static void Main()
+    {
+        Console.WriteLine("====================°C Degrees to °F Degrees====================");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("Enter temperature in °C: ");
+        double c = double.Parse(Console.ReadLine());
+        double f = c * 1.8 + 32;
+        Console.WriteLine("Temperature in Fahrenheit = {0}°F", f);
+    }
+}
