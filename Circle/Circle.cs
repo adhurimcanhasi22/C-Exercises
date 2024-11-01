eac20b02b04254bc26e855c28dcd95429274cbcd
adhurimcanhasi22 <121344747+adhurimcanhasi22@users.noreply.github.com>
2024-11-01 13:59:27 +0100

    Add files via upload

---
+++ Circle/Circle.cs
@@ -0,0 +1,19 @@
+﻿class Circle
+{
+    static void Main()
+    {
+        Console.WriteLine("====================AREA/PERIMETER OF CIRCLE====================");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("Enter the length of radius (cm)");
+        double r = double.Parse(Console.ReadLine());
+        double area = Math.PI * r * r;
+        double perimeter = Math.PI * r * 2;
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("Radius = {0}cm", r);
+        Console.WriteLine("Area of circle = {0}cm^2", area);
+        Console.WriteLine("Perimetre of circle = {0}cm", perimeter);
+    }
+}
---
+++ ComparingWords/ComparingWords.cs
@@ -0,0 +1,18 @@
+﻿class ComparingWords
+{
+	static void Main()
+	{
+		string word1 = Console.ReadLine();
+		word1 = word1.ToLower();
+		string word2 = Console.ReadLine();
+		word2 = word2.ToLower();
+		if (word1 == word2)
+		{
+			Console.WriteLine("Yes");
+		}
+		else
+		{
+			Console.WriteLine("No");
+		}
+	}
+}
