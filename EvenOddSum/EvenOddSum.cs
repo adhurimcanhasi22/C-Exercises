a19348139dabf90b8e95fee8f1137570d2b1837c
Adhurim Canhasi <121344747+adhurimcanhasi22@users.noreply.github.com>
2024-11-01 15:08:16 +0100

    Add files via upload

---
+++ EvenOddSum/EvenOddSum.cs
@@ -0,0 +1,29 @@
+﻿internal class even
+{
+    private static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int oddsum = 0;
+        int evensum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int element = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                evensum += element;
+            }
+            else
+            {
+                oddsum += element;
+            }
+        }
+        if (oddsum == evensum)
+        {
+            Console.WriteLine(oddsum);
+        }
+        else
+        {
+            Console.WriteLine(Math.Abs(oddsum - evensum));
+        }
+    }
+}
---
+++ ExcellentGrade/ExcellentGrade.cs
@@ -0,0 +1,11 @@
+﻿class ExcellentGrade
+{
+	static void Main()
+	{
+		double grade = double.Parse(Console.ReadLine());
+		if (grade > 5.50)
+		{
+			Console.WriteLine("Excellent!");
+		}
+	}
+}
