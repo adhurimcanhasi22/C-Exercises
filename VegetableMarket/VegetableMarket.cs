c7e0157a830b8dddf7c79270a73b3198cb9578bf
Adhurim Canhasi <121344747+adhurimcanhasi22@users.noreply.github.com>
2024-11-01 16:46:07 +0100

    Add files via upload

---
+++ VegetableMarket/VegetableMarket.cs
@@ -0,0 +1,16 @@
+﻿class VegetableMarket
+{
+	static void Main()
+	{
+		double vegprice = double.Parse(Console.ReadLine());
+		double fruitprice = double.Parse(Console.ReadLine());
+		int vegweight = int.Parse(Console.ReadLine());
+		int fruitweight = int.Parse(Console.ReadLine());
+
+		double vegcost = vegprice * vegweight;
+		double fruitcost = fruitprice * fruitweight;
+
+		double total = vegcost + fruitcost;
+		Console.WriteLine(total / 1.94);
+	}
+}
---
+++ VolleyBall/VolleyBall.cs
@@ -0,0 +1,27 @@
+﻿class VolleyBall
+{
+	static void Main()
+	{
+		string year = Console.ReadLine().ToLower();
+		int h = int.Parse(Console.ReadLine());
+		int w = int.Parse(Console.ReadLine());
+
+		int sofia = 48 - w;
+		double playsofia = (3.0 * sofia / 4) + (2.0 * h / 3);
+		double total = w + playsofia;
+
+		switch (year)
+		{
+			case "normal":
+				{
+					Console.WriteLine(Math.Floor(total));
+				}
+				break;
+			case "leap":
+				{
+					Console.WriteLine(Math.Floor(total * 15 / 100 + total));
+				}
+				break;
+		}
+	}
+}
