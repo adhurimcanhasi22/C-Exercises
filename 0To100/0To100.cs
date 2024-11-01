6a80ec6e829437824dfd1387616580ee4bd6f31e
adhurimcanhasi22 <121344747+adhurimcanhasi22@users.noreply.github.com>
2024-11-01 13:53:38 +0100

    Add files via upload

---
+++ 0To100/0To100.cs
@@ -0,0 +1,145 @@
+﻿int number = int.Parse(Console.ReadLine());
+float tens = number / 10;
+float units = number % 10;
+string unit1 = "one";
+string unit2 = "two";
+string unit3 = "three";
+string unit4 = "four";
+string unit5 = "five";
+string unit6 = "six";
+string unit7 = "seven";
+string unit8 = "eigt";
+string unit9 = "nine";
+if (number == 0)
+{
+	Console.WriteLine("Zero");
+}
+else if (number == 1)
+{
+	Console.WriteLine("One");
+}
+else if (number == 2)
+{
+	Console.WriteLine("Two");
+}
+else if (number == 3)
+{
+	Console.WriteLine("Three");
+}
+else if (number == 4)
+{
+	Console.WriteLine("Four");
+}
+else if (number == 5)
+{
+	Console.WriteLine("Five");
+}
+else if (number == 6)
+{
+	Console.WriteLine("Six");
+}
+else if (number == 7)
+{
+	Console.WriteLine("Seven");
+}
+else if (number == 8)
+{
+	Console.WriteLine("Eight");
+}
+else if (number == 9)
+{
+	Console.WriteLine("Nine");
+}
+else if (number == 10)
+{
+	Console.WriteLine("Ten");
+}
+
+else if (tens == 1.1 && units == 1)
+{
+	Console.WriteLine("Eleven");
+}
+else if (tens == 1.2 && units == 2)
+{
+	Console.WriteLine("Twelve");
+}
+else if (tens == 1.3 && units == 3)
+{
+	Console.WriteLine("Thirteen");
+}
+else if (tens == 1.4 && units == 4)
+{
+	Console.WriteLine("Fourteen");
+}
+else if (tens == 1.5 && units == 5)
+{
+	Console.WriteLine("Fifteen");
+}
+else if (tens == 1.6 && units == 6)
+{
+	Console.WriteLine("Sixteen");
+}
+else if (tens == 1.7 && units == 7)
+{
+	Console.WriteLine("Seventeen");
+}
+else if (tens == 1.8 && units == 8)
+{
+	Console.WriteLine("Eighteen");
+}
+else if (tens == 1.9 && units == 9)
+{
+	Console.WriteLine("Nineteen");
+}
+else if (tens == 2 && units == 0)
+{
+	Console.WriteLine("twenty");
+}
+else if (tens == 2 && units == 1)
+{
+	Console.WriteLine("twenty one");
+}
+else if (tens == 2 && units == 2)
+{
+	Console.WriteLine("twenty two");
+}
+else if (tens == 2 && units == 3)
+{
+	Console.WriteLine("twenty three");
+}
+else if (tens == 2 && units == 4)
+{
+	Console.WriteLine("twenty four");
+}
+else if (tens == 2 && units == 5)
+{
+	Console.WriteLine("twenty five");
+}
+else if (tens == 2 && units == 6)
+{
+	Console.WriteLine("twenty six");
+}
+else if (tens == 2 && units == 7)
+{
+	Console.WriteLine("twenty seven");
+}
+else if (tens == 2 && units == 8)
+{
+	Console.WriteLine("twenty eight");
+}
+else if (tens == 2 && units == 9)
+{
+	Console.WriteLine("twenty nine");
+}
+else if (tens == 3 && units == 1)
+{
+	Console.WriteLine("twenty one");
+}
+else if (tens == 3 && units == 0)
+{
+	Console.WriteLine("thirty");
+}
+
+
+
+
