861f4a1526a7deee1b3df71b8831ad73ef894d8f
Adhurim Canhasi <121344747+adhurimcanhasi22@users.noreply.github.com>
2024-11-01 15:41:22 +0100

    Add files via upload

---
+++ LeftRightSum/LeftRightSum.cs
@@ -0,0 +1,29 @@
+﻿internal class sum
+{
+    private static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int leftsum = 0;
+        int rightsum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            leftsum = leftsum + int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            rightsum = rightsum + int.Parse(Console.ReadLine());
+        }
+
+        if (leftsum == rightsum)
+        {
+            Console.WriteLine(leftsum);
+        }
+        else
+        {
+            int difference = Math.Abs(rightsum - leftsum);
+            Console.WriteLine(difference);
+        }
+    }
+}
---
+++ MaxNumber/MaxNumber.cs
@@ -0,0 +1,18 @@
+﻿internal class Max
+{
+    private static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int max = -1000000000;
+
+        for (int i = 1; i <= n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine(max);
+    }
+}
