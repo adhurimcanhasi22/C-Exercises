1c4fc14b4132e5dc39750b0fd24a4ed7b8f48c1f
Adhurim Canhasi <121344747+adhurimcanhasi22@users.noreply.github.com>
2024-11-01 16:42:48 +0100

    Add files via upload

---
+++ SummingUpSeconds/SummingUpSeconds.cs
@@ -0,0 +1,28 @@
+﻿class Seconds
+{
+	static void Main()
+	{
+		int sec1 = int.Parse(Console.ReadLine());
+		int sec2 = int.Parse(Console.ReadLine());
+		int sec3 = int.Parse(Console.ReadLine());
+		int totalsec = sec1 + sec2 + sec3;
+		int minutes = 0;
+		int secleft = totalsec;
+		if (secleft >= 60)
+		{
+			minutes++;
+			secleft -= 60;
+		}
+		if (secleft >= 60)
+		{
+			minutes++;
+			secleft -= 60;
+		}
+		Console.Write("{0}:", minutes);
+		if (secleft < 10)
+		{
+			Console.Write("0");
+		}
+		Console.WriteLine(secleft);
+	}
+}
---
+++ Time15/Time15.cs
@@ -0,0 +1,29 @@
+﻿class time
+{
+	static void Main()
+	{
+		int h = int.Parse(Console.ReadLine());
+		int m = int.Parse(Console.ReadLine());
+		int time = m + 15;
+		if (h < 23 && h >= 0)
+		{
+			if (time > 59)
+			{
+				h++;
+				time -= 60;
+				Console.WriteLine(h + ":" + "0" + time);
+			}
+			else if (time < 60)
+			{
+
+				Console.WriteLine(h + ":" + time);
+			}
+		}
+		else if (time > 59)
+		{
+			time -= 60;
+			Console.WriteLine("0:" + time);
+		}
+
+	}
+}
